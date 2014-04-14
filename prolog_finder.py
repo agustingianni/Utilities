b62ea077a7718e4c959cba2cb00ccea0f65511fb
anon <anon@ubuntu.(none)>
2014-04-14 09:10:42 -0300

    Pushed updates

--- prolog_finder.py
+++ prolog_finder.py
@@ -17,22 +17,63 @@ import idc
 import idautils
 import struct
 
-push_masks = [(0x0fff0000, 0x092d0000), (0x0fff0fff, 0x052d0004)]
-def is_push(opcode):
-	for mask in push_masks:
-		if (mask[0] & opcode) == mask[1]:
-			return True
+def find_prologs():
+	push_masks = [(0x0fff0000, 0x092d0000), (0x0fff0fff, 0x052d0004)]
+	def is_push(opcode):
+		for mask in push_masks:
+			if (mask[0] & opcode) == mask[1]:
+				return True
 
-	return False
+		return False
 
-image_start = idc.MinEA()
-image_end = idc.MaxEA()
-image_size = image_end - image_start
+	image_start = idc.MinEA()
+	image_end = idc.MaxEA()
+	image_size = image_end - image_start
 
-print "Analyzing image from %.8x-%.8x of size %.8x" % (image_start, image_end, image_size)
+	print "Analyzing image from %.8x-%.8x of size %.8x" % (image_start, image_end, image_size)
 
-for cur_ea in xrange(image_start, image_end, 4):
-	cur_long = Dword(cur_ea)
-	if is_push(cur_long):
-		print "Found candidate at 0x%.8x -> %s" % (cur_ea, GetDisasm(cur_ea))
+	for cur_ea in xrange(image_start, image_end, 4):
+		cur_long = Dword(cur_ea)
+		if is_push(cur_long):
+			print "Found candidate at 0x%.8x -> %s" % (cur_ea, GetDisasm(cur_ea))
 
+def find_push_thumb(mark=False):
+	def is_push(opcode):
+		return (0xfffffe00 & opcode) == 0x0000b400
+
+	image_start = idc.MinEA()
+	image_end = idc.MaxEA()
+	image_size = image_end - image_start
+
+	print "Analyzing image from %.8x-%.8x of size %.8x" % (image_start, image_end, image_size)
+
+	for cur_ea in xrange(image_start, image_end, 2):
+		cur_word = Word(cur_ea)
+		if is_push(cur_word) and "DCB" in GetDisasm(cur_ea):
+			print "Found  candidate at 0x%.8x -> %s" % (cur_ea, GetDisasm(cur_ea))
+			if mark:
+				MakeCode(cur_ea)
+				print "Marked candidate at 0x%.8x -> %s" % (cur_ea, GetDisasm(cur_ea))
+
+
+def find_nops(mark=False):
+	"""
+	Look for THUMB nops (0xbf00).
+	"""
+	def is_nop(opcode):
+		return opcode == 0xbf00
+
+	image_start = idc.MinEA()
+	image_end = idc.MaxEA()
+	image_size = image_end - image_start
+
+	print "Analyzing image from %.8x-%.8x of size %.8x" % (image_start, image_end, image_size)
+
+	for cur_ea in xrange(image_start, image_end, 2):
+		cur_word = Word(cur_ea)
+		if is_nop(cur_word):
+			print "Found candidate at 0x%.8x -> %s" % (cur_ea, GetDisasm(cur_ea))
+			if mark:
+				MakeCode(cur_ea)
+
+find_push_thumb(mark=False)
